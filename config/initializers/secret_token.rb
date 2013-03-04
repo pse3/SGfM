6ca184dbf5f6583a0d3e54d7e020cb2d91ef2601
Kenneth Radunz <kenneth.radunz@web.de>
2013-03-04 14:15:50 +0100

    Initial Setup Commit. Created a RoR project without ActiveRecord. Additionally, the mongoid config files was generated. Several Gems were added to the Gemfile and the bootstrap assets are included. Certain files and folders are igonered by a custom gitignore file.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SGfM::Application.config.secret_token = 'ce939c656586a2f3350218040e683c06c7f082036048cda08f00057e230e62525d3a0922f7f51092ac642349d3633b3370eaf9a153b32a240c37214b72ef3bc6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SGfM::Application.config.session_store :cookie_store, key: '_SGfM_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SGfM::Application.config.session_store :active_record_store
