d9f421c535ff67432234c90ccaaa3d47e66a80e2
Kenneth Radunz <kenneth.radunz@web.de>
2013-03-11 15:25:23 +0100

    fixed a little bug. I hope

--- app/models/actor_type.rb
+++ app/models/actor_type.rb
@@ -5,5 +5,5 @@ class ActorType
   field :name, :type => String
 
   has_many :actors, class_name: 'Actor', inverse_of: :actor_type             #referenced
-  has_and_belongs_to :information_type, class_name: 'InformationType'    #referenced
+  has_and_belongs_to_many :information_type, class_name: 'InformationType'    #referenced
 end
