0baa6a98e6ef02e115030df57c3b951551b173f9
Oliver Stapleton <oliver.stapleton@students.unibe.ch>
2013-03-13 23:54:17 +0100

    tried fixing mapping between information and information_type

--- app/models/information.rb
+++ app/models/information.rb
@@ -8,9 +8,9 @@ class Information
   field :created_at, :type => DateTime
   field :changed_a, :type => DateTime
 
-  belongs_to :creator, class_name: 'User', inverse_of: nil          #referenced  / one way relationship                                                                                                                                                                                                                                                 #embedded
-  has_one :information_type, class_name: 'InformationType'          #referenced
-  has_one :scope, class_name: 'Scope'                               #referenced
+  belongs_to :creator, class_name: 'User', inverse_of: nil                              #referenced  / one way relationship                                                                                                                                                                                                                                                 #embedded
+  belongs_to :information_type, class_name: 'InformationType', inverse_of: nil          #referenced  / one way relationship
+  has_one :scope, class_name: 'Scope'                                                   #referenced
 
   def initialize
     super
