e1edd0892c77619de2a58dd022ca3047e2fbd0f3
Urs Zysset <UrsZysset@gmail.com>
2013-03-13 20:45:14 +0100

    added actor_type_by_key HelperMethod

--- app/helpers/actor_type_helper.rb
+++ app/helpers/actor_type_helper.rb
@@ -4,6 +4,10 @@ module ActorTypeHelper
     @actor_types = ActorType.each
   end
 
+  def actor_type_by_key(key)
+    @actor_type.find_by_key(key)
+  end
+
   # WTF is this shit?
   def list_information_types_of_actor_type
     @information_types_of_actor_type = ActorType.InformationType
