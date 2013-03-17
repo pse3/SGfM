867249d50fe37412b0995c093cbd15cca37b71de
Urs Zysset <UrsZysset@gmail.com>
2013-03-17 22:16:23 +0100

    Theres only one way to solve merge conflicts! reset --hard!

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -9,25 +9,22 @@ class Actor
   belongs_to :actor_type, class_name: 'ActorType', inverse_of: nil                   #referenced
   embeds_many :informations, class_name: 'Information'                                  #embedded
   belongs_to :owner, class_name: 'User'                                                 #embedded
+
 	validates :informations, informations_not_empty: true
+
+
   def initialize
     super
     self.created_at = DateTime.now
   end
 
-  #TODO refactor...
   def find_information_by_key(key)
-    infos_by_type = self.informations.select { |info|
-      info.information_type.key == key
-    }
-    infos_by_type.first unless infos_by_type.empty?
+    self.informations.detect{ |info| info.information_type.key == key }
   end
 
   def to_s
-    unless find_information_by_key(:company).nil?
-       return find_information_by_key(:company)
-    end
-    return "#{self.find_information_by_key(:last_name)} #{find_information_by_key(:first_name)}"
+    return find_information_by_key(:company) unless find_information_by_key(:company).nil?
+    return self.find_information_by_key(:last_name).to_s + ' ' + find_information_by_key(:first_name).to_s
   end
 
 end
