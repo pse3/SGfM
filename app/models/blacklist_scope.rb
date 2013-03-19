4d49403492b598930b7890065db241eed6101fab
Arun Sittampalam <arun@sittampalam.ch>
2013-03-19 20:02:39 +0100

    A first version of BlacklistScope. Untested.

--- app/models/blacklist_scope.rb
+++ app/models/blacklist_scope.rb
@@ -1,6 +1,20 @@
 class BlacklistScope < Scope
 	def visible? (viewer, viewed)
+		list = self.list
+		visible = true
+		list.each do |disallowed_viewer|
+			if allowed_viewer == :self
+				currently_visible = !(viewer == viewed) #current_visibility means visibility to current "allowed_viewer"
+			else
+				currently_visible = !(viewer.user_type == allowed_viewer)
+			end
+			visible = visible and currently_visible
 
+			unless visible
+				break
+			end
+		end
+		end
 	end
 end
 
