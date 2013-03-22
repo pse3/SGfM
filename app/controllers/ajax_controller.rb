41e9eb2fe7d1b6952982c691199d137dfdaec99e
Urs Zysset <UrsZysset@gmail.com>
2013-03-22 20:05:01 +0100

    relationship field now removeable in form

--- app/controllers/ajax_controller.rb
+++ app/controllers/ajax_controller.rb
@@ -1,7 +1,8 @@
 class AjaxController < ApplicationController
 
   def add_relationship_form
-    render(:partial => 'actor/add_relationship_form', :locals => {:all_actors => Actor.all})
+    render(:partial => 'actor/add_relationship_form', :locals => {  :all_actors => Actor.all,
+                                                                    :field_number => params[:field_number].to_i })
   end
 
 end
