be1ad3603352738fcb43f3fa3ff12e9f23c03862
Lafunamor <info@zipfuzapfu.ch>
2013-03-24 16:39:35 +0100

    added FieldType in InformationType list
    added localized Name field to InformationFieldType

--- app/models/information_field_type.rb
+++ app/models/information_field_type.rb
@@ -2,16 +2,13 @@ class InformationFieldType
   include Mongoid::Document
 
   field :key, :type => Symbol
+  field :name, :type => String, :localize => true
 
   validates_uniqueness_of :key
 
 
   def self.find_by_key(key)
     InformationFieldType.find_by(key: key)
-  end
-
-  def name
-    self[:key].to_s
   end
 
 end
