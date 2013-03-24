4701ff7aa5614209883839981c7278a7ee5c78b4
Lafunamor <info@zipfuzapfu.ch>
2013-03-24 19:26:42 +0100

    added FieldType in InformationType list
    added localized Name field to InformationFieldType

---
+++ app/models/information_field_multiple_select.rb
@@ -0,0 +1,18 @@
+class InformationFieldMultipleSelect < InformationFieldType
+
+  def value(value)
+    value.to_s
+  end
+
+  def value=(value)
+    value
+  end
+
+  def form(form_helper, key, options = { :data => [], :required => false, :value => '' })
+    form_helper.select(key, options[:data], {:promt => 'select'},
+                      { :required => options[:required],
+                       :selected  => options[:value],
+                       :multiple => true})
+  end
+
+end
