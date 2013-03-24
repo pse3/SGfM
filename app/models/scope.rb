e174cecc267a5fbd1fde7ae497519096c3370411
Arun Sittampalam <arun@sittampalam.ch>
2013-03-24 23:15:06 +0100

    Solved issue where scope creation wasn't running, when there where no other scopes. (Because of Scope.all.distinct('_type') - now working with rails descendant tracking).

--- app/models/scope.rb
+++ app/models/scope.rb
@@ -11,9 +11,8 @@ class Scope
 
   #no mappings needed! (yet)
 
-	#Returns all types of scopes in a list.
+	#Returns all types of scopes in a list. (That means it returns the Class-Objects)
 	def self.all_types
-		#TODO maybe using '_type' directly isn't really beautiful - Might have to 'implement' a separate field for that. (pro: localizability)
-		Scope.all.distinct("_type")
+		 Scope.descendants
 	end
 end
