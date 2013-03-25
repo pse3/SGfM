a2dc8851df99c4d6093092971c448e0349629fc1
Kenneth Radunz <kenneth.radunz@web.de>
2013-03-25 16:46:12 +0100

    Moved the Scoped module to a new file.

--- app/models/information.rb
+++ app/models/information.rb
@@ -1,28 +1,3 @@
-module Scoped
-
-  def self.included(base)
-    base.extend ClassMethods
-    base.belongs_to :scope, class_name: 'Scope', inverse_of: nil
-  end
-
-  def set_scope(child)
-    child.scope=scope if scope
-  end
-
-  module ClassMethods
-    def inherit_scope_from(parent)
-      method_name = "#{parent}="
-      orig_method_name = "orig_#{method_name}"
-      alias_method orig_method_name.to_sym, method_name.to_sym
-      define_method(method_name) do |param|
-        #add some callback
-        self.send(orig_method_name, param)
-        param.set_scope(self)
-      end
-    end
-  end
-end
-
 class Information
   # base class for the actual data
 
