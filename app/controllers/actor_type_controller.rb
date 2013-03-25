2aa23a09910c2ad198e431e204989f053ce7a291
Oliver Stapleton <oliver.stapleton@students.unibe.ch>
2013-03-25 16:21:19 +0100

    furtehr functionality

--- app/controllers/actor_type_controller.rb
+++ app/controllers/actor_type_controller.rb
@@ -1,20 +1,23 @@
 class ActorTypeController < ApplicationController
 
 	before_filter :authenticate_login!, :authenticate_admin!
-
+  #TODO refactoorrrrr
 	# Creates an ActorType with chosen name and InformationTypes
   def create
     @actor_type = ActorType.new
     @actor_type.key = params[:actor_type][:name][:en].downcase.tr(' ', '_')
-
+    index = 0 #dummy variable to test functionality
     @actor_type.name_translations = params[:actor_type][:name]
     unless params[:information].nil?
       params[:information].each do |key,value|
         info_type = InformationType.find_by_key(key.to_sym)
         if value.to_i == 1
+          index = index + 1
           info_type_decorator = InformationTypeDecorator.new
-          info_type_decorator.required = true
           info_type_decorator.information_type = info_type
+          info_type_decorator.required = true
+          info_type_decorator.index = index
+          info_type_decorator.searchable = true
           info_type_decorator.save
           @actor_type.information_type.push(info_type_decorator)
         end
--- app/models/information_type_decorator.rb
+++ app/models/information_type_decorator.rb
@@ -2,6 +2,8 @@ class InformationTypeDecorator
   include Mongoid::Document
 
   field :required, type: Boolean
+  field :searchable, type: Boolean
+  field :index, type: Integer
 
   belongs_to :information_type, class_name: 'InformationType', inverse_of: nil
 
