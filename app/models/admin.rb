df78b91c8a7648670013b3b5f9a2e0378a3423b9
RB <rafael.breu@students.unibe.ch>
2013-04-04 12:03:45 +0200

    Admins can add actors now

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,8 +1,13 @@
 class Admin
+
   include Mongoid::Document
+
   has_one :login, :as => :account
 
-	def user_type
-		self.login.account_type
-	end
+  has_many :actors, class_name: 'Actor', inverse_of: :owner             #referenced
+
+  def user_type
+    self.login.account_type
+  end
+
 end
