ab0de5e043c0487f6e0bcfd11c0c148e24914222
Kenneth Radunz <kenneth.radunz@gmail.com>
2013-04-08 19:41:04 +0200

    fixed several bugs. There is still one bug I know of: The edit view fails if data is not set

--- app/controllers/information_type_controller.rb
+++ app/controllers/information_type_controller.rb
@@ -7,8 +7,8 @@ class InformationTypeController < ApplicationController
     information_type = InformationType.new
     information_type.name_translations = params[:information_type][:name]
     information_type.key = params[:information_type][:key]
-    if params[:information_type][:scope]
-			information_type.scope =  Scope.find_by name: params[:information_type][:scope]
+    if params[:information_type][:scope].size > 0
+			information_type.scope =  Scope.find_by key: params[:information_type][:scope]
     end
     information_type.information_field_type= InformationFieldType.find_by_key params[:information_type][:information_field_type]
     #TODO make the view receive the data form via ajax so it is only displayed when needed
@@ -38,7 +38,9 @@ class InformationTypeController < ApplicationController
   end
 
   def update
-    information_type = InformationType.find_by id: params[:id]
+    information_type = InformationType.find_by(id: params[:id])
+    params[:information_type][:information_field_type] = InformationFieldType.find_by_key params[:information_type][:information_field_type]
+    params[:information_type][:scope] = Scope.find_by key: params[:information_type][:scope] if params[:information_type][:scope].size > 0  # dont check empty string
     information_type.update_attributes(params[:information_type])
     information_type.name_translations = params[:information_type][:name]
     information_type.data_translations = information_type.information_field_type.parse_data(params[:information_type][:information_field_type_data])
