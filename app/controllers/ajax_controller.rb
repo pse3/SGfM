eae6f9a7458375366290c9181346ab7916830988
Kenneth Radunz <kenneth.radunz@web.de>
2013-04-10 13:25:58 +0200

    working on dynamic data option view

--- app/controllers/ajax_controller.rb
+++ app/controllers/ajax_controller.rb
@@ -8,6 +8,10 @@ class AjaxController < ApplicationController
     render_partial('actor_type/add_information_type_form', {:information_types => InformationType.all})
   end
 
+  def information_type_data_form
+    information_type = InformationType.find_by id: params[:information_field_type]
+    render :partial => InformationTypeHelper.get_partial(information_type)
+  end
 
   private
   def render_partial(file, locals = {})
--- app/helpers/information_type_helper.rb
+++ app/helpers/information_type_helper.rb
@@ -7,4 +7,15 @@ module InformationTypeHelper
   def information_field_types
     InformationFieldType.all
   end
+
+  @mapping = {
+      InformationFieldText => 'information_type/data/no_options',
+      InformationFieldEmail => 'information_type/data/no_options',
+      InformationFieldSingleSelect => 'information_type/data/localized_options',
+      InformationFieldMultipleSelect => 'information_type/data/localized_options'
+  }
+
+  def self.get_partial(information_type)
+    @mapping[information_type.class] || 'information_type/data/no_options'
+  end
 end
