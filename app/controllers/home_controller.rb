aa19b364f67d6fa9b508f708ae5bdb5efe76ce52
Arun Sittampalam <arun@sittampalam.ch>
2013-04-21 20:52:31 +0200

    Once you are signed in, root ("/") now links to either your actors or the admin panel. Devise routes to root after sign in now (instead of logins#show).

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,14 @@
 class HomeController < ApplicationController
 
   def index
-    @logins = Login.all
+		if current_login.nil?
+   		@logins = Login.all
+			return
+		elsif current_login.is_admin?
+			redirect_to actor_types_path
+		else
+			redirect_to actors_path
+		end
   end
 
 end
