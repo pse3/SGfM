fcd30eddc4ede1c6cd84189ae8719fe28222a050
Lafunamor <info@zipfuzapfu.ch>
2013-04-21 16:52:43 +0200

    mailing fixed

--- app/mailers/devise_mailer.rb
+++ app/mailers/devise_mailer.rb
@@ -2,11 +2,19 @@ class DeviseMailer < Devise::Mailer
   protected
 
   def default_url_options(options={})
-    { :locale => I18n.locale }
+    { :locale => I18n.locale,
+    :host => 'localhost:3000'}
   end
 
   def devise_mail(*args)
     default_url_options
+
     super
   end
+
+  def invitation(user)
+    @user = user
+    mail(:to => user.email, :subject => "Welcome!")
+  end
+
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,4 +61,16 @@ SGfM::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+      :enable_starttls_auto => true,
+      :address => "smtp.gmail.com",
+      :port => 587,
+      :domain => "gmail.com",
+      :authentication => :login,
+      :user_name => "xx@gmail.com",
+      :password => "pw",
+  }
+
 end
