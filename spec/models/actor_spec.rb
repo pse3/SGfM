defa2c5a76584cdbad4fe3343f69fe4fa6cddd6d
Rafael Breu <rafael.breu@bluewin.ch>
2013-04-29 16:46:50 +0200

    now actor is constructed correctly in the actors test

--- spec/models/actor_spec.rb
+++ spec/models/actor_spec.rb
@@ -1,32 +1,26 @@
 require 'spec_helper'
 
 describe Actor do
-  before {
-
-    #create an information_field_type
-    information_field_text = InformationFieldText.new
-    information_field_text.key = :text_test
-    information_field_text.name_translations = { :en => 'Text field', :de => 'Textfeld', :it => '???', :fr => '???' }
-    information_field_text.save
-
-    info_company = InformationType.new
-    info_company.key = :company_test
-    info_company.information_field_type = information_field_text
-    info_company.name_translations = { :en => 'Company', :de =>'Firma', :it => '??', :fr => '??' }
-    info_company.save
+  before do
 
     #create a scope
     scope_public = BlacklistScope.new
     scope_public.key = :public_test
-    scope_public.name = 'public'
+    scope_public.name_translations = { :en => 'public', :de => 'öffentlich', :it => 'pubblico', :frc=> 'public' }
     scope_public.list = []
     scope_public.save
+
+    #create an information_field_type
+    information_field_text = InformationFieldText.new
+    information_field_text.key = :text
+    information_field_text.name_translations = { :en => 'Text field', :de => 'Textfeld', :it => '???', :fr => '???' }
+    information_field_text.save
 
     #create some information_types
     itype_name = InformationType.new
     itype_name.key = :name_test
     itype_name.information_field_type = information_field_text
-    itype_name.name = "Name"
+    itype_name.name_translations = { :en => 'First name', :de =>'Vorname', :it => '??', :fr => '??' }
     itype_name.scope = scope_public
     itype_name.save
 
@@ -37,49 +31,60 @@ describe Actor do
     itype_phone.scope = scope_public
     itype_phone.save
 
-    itype_address = InformationType.new
-    itype_address.key = :address_test
-    itype_address.information_field_type = information_field_text
-    itype_address.name = "Adresse"
-    itype_address.scope = scope_public
-    itype_address.save
-
-    itype_email = InformationType.new
-    itype_email.key = :email_test
-    itype_email.information_field_type = information_field_text
-    itype_email.name = "Email"
-    itype_email.scope = scope_public
-    itype_email.save
-
     #create some actor_types
     @atype_doctor = ActorType.new
-    @atype_doctor.name = "Arzt"
     @atype_doctor.key = :doctor_test
-    @itypedec_name = InformationTypeDecorator.create(itype_name, @atype_doctor, true, true)
-    @itypedec_phone = InformationTypeDecorator.create(itype_phone, @atype_doctor, true, true)
-    @atype_doctor.to_string_pattern = "|:name|//|:phone|"
+    InformationTypeDecorator.create(itype_name, @atype_doctor, true, true)
+    InformationTypeDecorator.create(itype_phone, @atype_doctor, true, true)
+    @atype_doctor.name_translations = { :en => 'Doctor', :de =>'Arzt', :it => 'Dottore', :fr => 'Médecin' }
+    @atype_doctor.to_string_pattern = "|:name_test|//|:phone_test|"
     @atype_doctor.save
+
+    #create a user
+    @user = User.new
+    login = Login.new(:email => 'email5@domain.ch',
+                      :password => 'test1234',
+                      :password_confirmation => 'test1234')
+    login.account = @user
+    @user.save
 
     #create actor
     @actor = Actor.new
-    @actor.owner = @user1
     @actor.actor_type = @atype_doctor
-  }
 
+    actor_name = Information.new
+    actor_name.scope = scope_public
+    actor_name.information_type_decorator = @actor.actor_type.decorator_by_key(:name_test)
+    actor_name.value = 'Name of our test actor'
+    actor_name.actor = @actor
 
-  it "creates an actor" do
-    @actor.should_not be_nil
-    @actor.should respond_to(:actor_type)
-    @actor.should respond_to(:informations)
+    actor_phone = Information.new
+    actor_phone.scope = scope_public
+    actor_phone.information_type_decorator = @actor.actor_type.decorator_by_key(:phone_test)
+    actor_phone.value = '033 666 77 88'
+    actor_phone.actor = @actor
+
+    @user.actors.push(@actor)
+    @actor.save
+    @user.save
+
   end
+
+  subject{@actor}
+
+  it{should be_valid}
+  it{should_not be_nil}
+  it{should respond_to(:actor_type)}
+  it{should respond_to (:informations)}
+
+  let(:save){@actor.save}
+  specify {save.should be_true}
+
+  let(:actor_type) { @actor.actor_type }
+  specify {actor_type.should eq(@atype_doctor)}
 
   it "sets correct time of creation when initializing" do
     @actor.created_at.should be_within(10).of(DateTime.now)
-  end
-
-  it "assigns actor_type to actor" do
-    @actor.actor_type = @atype_doctor
-    @actor.actor_type.should equal(@atype_doctor)
   end
 
   it "adds an information to actor" do
