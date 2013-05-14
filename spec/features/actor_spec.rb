1c01b0cb35baca9cb8f13a61fc628e876546df72
Rafael Breu <rafael.breu@bluewin.ch>
2013-05-14 15:36:21 +0200

    Added Selenium to Gemfile for javascript tests. Added first create-actor-test... however, it does not work, because two next buttons exist on the page

--- spec/features/actor_spec.rb
+++ spec/features/actor_spec.rb
@@ -8,7 +8,7 @@ describe "actor spec" do
     visit '/'
     click_link("Sign up")
     within('#new_login') do
-      fill_in 'login_email', :with => 'test@test.de'
+      fill_in 'login_email', :with => 'test_12@test.de'
       fill_in 'login_password', :with => '1234test'
       fill_in 'login_password_confirmation', :with => '1234test'
       click_button 'Sign up'
@@ -16,17 +16,21 @@ describe "actor spec" do
   end
 
   context "with full information" do
-    it "creates new doctor" do
-      click_on 'Next'
-      select('gynecologist', :from => 'Select Box')
-      fill_in "actor_information_first_name", :with => 'TestFirstname'
-      fill_in "actor_information_last_name", :with => 'TestLastname'
-      fill_in "actor_information_street", :with => 'TestStreet'
-      fill_in "actor_information_street_number", :with => '666'
-      fill_in "actor_information_zip", :with => '9032'
-      click_button 'Next'
-      click_button 'Create'
-      page.should contain("alert alert-success fade in")
+    it "creates new doctor",:js => true do
+
+      find('div')['#accordion-inner'].click_link("Next")
+      #within('#actor-type') do
+      #  click_link 'Next'
+      #end
+      select('gynecologist', :from => 'actor_information_medical_specialisations')
+      #fill_in "actor_information_first_name", :with => 'TestFirstname'
+      #fill_in "actor_information_last_name", :with => 'TestLastname'
+      #fill_in "actor_information_street", :with => 'TestStreet'
+      #fill_in "actor_information_street_number", :with => '666'
+      #fill_in "actor_information_zip", :with => '9032'
+      #click_button 'Next'
+      #click_button 'Create'
+      #page.should contain("alert alert-success fade in")
     end
   end
 end
