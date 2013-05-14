7c318f112c4cda07b14abecee9ec7d4ef7fc60f4
Rafael Breu <rafael.breu@bluewin.ch>
2013-05-14 14:15:37 +0200

    added login_spec.rb

---
+++ spec/features/login_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+Capybara.match = :first
+
+describe "Log in" do
+
+  before do
+    visit '/'
+    click_link("Sign up")
+    within('#new_login') do
+      fill_in 'login_email', :with => 'test@test.de'
+      fill_in 'login_password', :with => '1234test'
+      fill_in 'login_password_confirmation', :with => '1234test'
+      click_button 'Sign up'
+    end
+    click_on 'Sign out'
+  end
+
+  context "with existing login" do
+    it "logs in" do
+      click_on 'Sign in'
+      fill_in "login_email", :with => 'test@test.de'
+      fill_in "login_password", :with => '1234test'
+      click_button 'Sign in'
+      page.should have_selector('a', text: 'Sign out')
+    end
+  end
+end
