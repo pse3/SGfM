835374ab0b9947a2818bb4e10735e30a03288b70
Rafael Breu <rafael.breu@bluewin.ch>
2013-05-14 16:25:45 +0200

    still not solved the problem, that sometimes selenium is too fast for our Javascript...

--- spec/features/actor_spec.rb
+++ spec/features/actor_spec.rb
@@ -6,18 +6,24 @@ describe "actor spec" do
 
   before do
     visit '/'
-    click_link("Sign up")
-    within('#new_login') do
-      fill_in 'login_email', :with => 'test_12@test.de'
-      fill_in 'login_password', :with => '1234test'
-      fill_in 'login_password_confirmation', :with => '1234test'
-      click_button 'Sign up'
-    end
   end
 
   context "with full information" do
     it "creates new doctor",:js => true do
+
+      click_link("Sign up")
+      within('#new_login') do
+        fill_in 'login_email', :with => 'test_12@test.de'
+        fill_in 'login_password', :with => '1234test'
+        fill_in 'login_password_confirmation', :with => '1234test'
+        click_button 'Sign up'
+      end
+
+      #page.should have_content('Doctor')
       page.should have_css('#actor-type-select')
+      #page.should have_css('#actor-type')
+      #page.should have_css('#new-actor')
+
       within('#actor-type') do
         page.should have_selector('a', text: 'Next')
         click_link 'Next'
@@ -32,7 +38,50 @@ describe "actor spec" do
       click_link 'Next'
       click_button 'Create'
       page.should have_content("Doctor")
+      click_link 'Sign out'
     end
+  end
+
+  context "with incomplete information" do
+    it "does not create an actor",:js => true do
+
+      click_link("Sign up")
+      within('#new_login') do
+        fill_in 'login_email', :with => 'test_13@test.de'
+        fill_in 'login_password', :with => '1234test'
+        fill_in 'login_password_confirmation', :with => '1234test'
+        click_button 'Sign up'
+      end
+
+      #page.should have_content('Doctor')
+      page.should have_css('#actor-type-select')
+      #page.should have_css('#actor-type')
+      #page.should have_css('#new-actor')
+
+      within('#actor-type') do
+        page.should have_selector('a', text: 'Next')
+        click_link 'Next'
+      end
+
+      select('gynecologist', :from => 'actor_information_medical_specialisations')
+      fill_in "actor_information_first_name", :with => 'TestFirstname'
+      fill_in "actor_information_street", :with => 'TestStreet'
+      fill_in "actor_information_street_number", :with => '666'
+      fill_in "actor_information_zip_code", :with => '9032'
+      click_link 'Next'
+      click_button 'Create'
+      page.should_not have_content("Doctor")
+      click_link 'Sign out'
+    end
+  end
+
+  context "with full information" do
+     it "is updated", :js => true do
+       click_on 'Sign in'
+       fill_in "login_email", :with => 'test_13@test.de'
+       fill_in "login_password", :with => '1234test'
+       click_button 'Sign in'
+     end
   end
 
 
