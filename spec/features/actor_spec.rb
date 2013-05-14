ee98ab3d217111cf383cf8be60d5f77b52cea468
Rafael Breu <rafael.breu@bluewin.ch>
2013-05-14 20:48:11 +0200

    added test that assures that with incomplete information no actor is created

--- spec/features/actor_spec.rb
+++ spec/features/actor_spec.rb
@@ -427,7 +427,7 @@ describe "actor spec" do
   end
 
   context "with full information" do
-    it "creates new doctor",:js => true do
+    it "creates a new doctor",:js => true do
 
       click_link("Sign up")
       within('#new_login') do
@@ -449,9 +449,38 @@ describe "actor spec" do
       fill_in "actor_information_zip_code", :with => '9032'
       click_link 'Next'
       click_button 'Create'
-      page.should have_content("Doctor")
+      page.should have_content("successfully")
       click_link 'Sign out'
     end
+  end
+  context "with incomplete information" do
+    it "does not create a new doctor",:js => true do
+
+      click_link("Sign up")
+      within('#new_login') do
+        fill_in 'login_email', :with => 'test_12@test.de'
+        fill_in 'login_password', :with => '1234test'
+        fill_in 'login_password_confirmation', :with => '1234test'
+        click_button 'Sign up'
+      end
+      sleep(1)
+      within('#actor-type') do
+        click_link 'Next'
+      end
+
+      select('gynecologist', :from => 'actor_information_medical_specialisations')
+      fill_in "actor_information_first_name", :with => 'TestFirstname'
+      fill_in "actor_information_street", :with => 'TestStreet'
+      fill_in "actor_information_street_number", :with => '666'
+      fill_in "actor_information_zip_code", :with => '9032'
+      click_link 'Next'
+      click_button 'Create'
+      page.should_not have_content("successfully")
+      click_link 'Sign out'
+    end
+  end
+  context "with full information" do
+
   end
 
 
