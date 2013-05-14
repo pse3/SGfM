732af538ebfe6f7b4991801a3227d52754e7daa4
Rafael Breu <rafael.breu@bluewin.ch>
2013-05-14 16:11:03 +0200

    adding an actor working now with help of Selenium

--- spec/features/actor_spec.rb
+++ spec/features/actor_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-Capybara.match = :first
+#Capybara.match = :first
 
 describe "actor spec" do
 
@@ -17,20 +17,23 @@ describe "actor spec" do
 
   context "with full information" do
     it "creates new doctor",:js => true do
+      page.should have_css('#actor-type-select')
+      within('#actor-type') do
+        page.should have_selector('a', text: 'Next')
+        click_link 'Next'
+      end
 
-      find('div')['#accordion-inner'].click_link("Next")
-      #within('#actor-type') do
-      #  click_link 'Next'
-      #end
       select('gynecologist', :from => 'actor_information_medical_specialisations')
-      #fill_in "actor_information_first_name", :with => 'TestFirstname'
-      #fill_in "actor_information_last_name", :with => 'TestLastname'
-      #fill_in "actor_information_street", :with => 'TestStreet'
-      #fill_in "actor_information_street_number", :with => '666'
-      #fill_in "actor_information_zip", :with => '9032'
-      #click_button 'Next'
-      #click_button 'Create'
-      #page.should contain("alert alert-success fade in")
+      fill_in "actor_information_first_name", :with => 'TestFirstname'
+      fill_in "actor_information_last_name", :with => 'TestLastname'
+      fill_in "actor_information_street", :with => 'TestStreet'
+      fill_in "actor_information_street_number", :with => '666'
+      fill_in "actor_information_zip_code", :with => '9032'
+      click_link 'Next'
+      click_button 'Create'
+      page.should have_content("Doctor")
     end
   end
+
+
 end
