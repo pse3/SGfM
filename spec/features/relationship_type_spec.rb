fcbd4efcbf8e76f85f926d988df694d411d3c8b2
Rafael Breu <rafael.breu@bluewin.ch>
2013-05-16 10:50:43 +0200

    relationship_type tests working fine now

--- spec/features/relationship_type_spec.rb
+++ spec/features/relationship_type_spec.rb
@@ -29,6 +29,18 @@ describe "Scope" do
     scope_public.list = []
     scope_public.save
 
+    # Create some RelationshipTypes
+    relation_works_with = RelationshipType.new
+    relation_works_with.key = :works_with
+    relation_works_with.name_translations = { :en => 'works with', :de =>'arbeitet mit', :it => '??', :fr => '??' }
+    relation_works_with.question_translations = {
+        :en => 'With whom do you work?',
+        :de => 'Mit wem arbeitet Ihr?',
+        :it => '??',
+        :fr => '??'
+    }
+    relation_works_with.save
+
     #creates an admin
 
     admin = Admin.new
@@ -42,25 +54,23 @@ describe "Scope" do
     visit '/'
   end
 
-  it 'creates a relationship type'
-  it 'updates a relationship type'
-  it 'destroys a relationship type'
-
   it 'is created', :js => true do
     click_on 'Sign in'
     fill_in "login_email", :with => 'admin@domain.ch'
     fill_in "login_password", :with => 'test1234'
     click_button 'Sign in'
-    click_on 'Scopes'
-    click_on 'Create new Scope'
-    select 'WhitelistScope', :from => 'scope__type'
-    fill_in 'scope_name[en]', :with => 'TestScopeEnglish'
-    fill_in 'scope_name[de]', :with => 'TestScopeGerman'
-    fill_in 'scope_name[fr]', :with => 'TestScopeFrench'
-    fill_in 'scope_name[it]', :with => 'TestScopeItalian'
-    fill_in 'scope_key', :with => 'TestScopeKey'
+    click_on 'RelationshipTypes'
+    click_on 'Create new RelationshipType'
+    fill_in 'relationship_type_name_translations[en]', :with => 'TestRelationshipTypeEnglish'
+    fill_in 'relationship_type_name_translations[de]', :with => 'TestRelationshipTypeGerman'
+    fill_in 'relationship_type_name_translations[fr]', :with => 'TestRelationshipTypeFrench'
+    fill_in 'relationship_type_name_translations[it]', :with => 'TestRelationshipTypeItalian'
+    fill_in 'relationship_type_question_translations[en]', :with => 'TestRelationshipTypeQuestionEnglish'
+    fill_in 'relationship_type_question_translations[de]', :with => 'TestRelationshipTypeQuestionGerman'
+    fill_in 'relationship_type_question_translations[fr]', :with => 'TestRelationshipTypeQuestionFrench'
+    fill_in 'relationship_type_question_translations[it]', :with => 'TestRelationshipTypeQuestionItalian'
     click_on 'Create'
-    page.should have_content('TestScopeEnglish')
+    page.should have_content('TestRelationshipTypeEnglish')
     page.should have_content('uccessfully')
   end
 
@@ -69,14 +79,14 @@ describe "Scope" do
     fill_in "login_email", :with => 'admin@domain.ch'
     fill_in "login_password", :with => 'test1234'
     click_button 'Sign in'
-    click_on 'Scopes'
-    page.should have_content("public")
+    click_on 'RelationshipTypes'
+    page.should have_content("works with")
     page.first(".icon-edit").click
-    fill_in 'edited_scope_name[en]', :with => 'UpdatedTestScopeName'
-    click_on 'Save'
+    fill_in 'relationship_type_name_translations[en]', :with => 'NewEnglishRelationshipTypeName'
+    click_on 'update'
     page.should have_content("uccessfully")
-    page.should have_content("UpdatedTestScopeName")
-    page.should_not have_content("TestScopeEnglish")
+    page.should have_content("NewEnglishRelationshipTypeName")
+    page.should_not have_content("works with")
     click_on 'Sign out'
   end
 
@@ -85,13 +95,13 @@ describe "Scope" do
     fill_in "login_email", :with => 'admin@domain.ch'
     fill_in "login_password", :with => 'test1234'
     click_button 'Sign in'
-    click_on 'Scopes'
-    page.should have_content('public')
+    click_on 'RelationshipTypes'
+    page.should have_content('works with')
     page.first(".icon-trash").click
     page.driver.browser.switch_to.alert.accept
     page.should have_content("uccessfully")
     page.should have_content("deleted")
-    page.should_not have_content("public")
+    page.should_not have_content("works with")
     click_on 'Sign out'
   end
 
