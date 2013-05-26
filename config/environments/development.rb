2685bb423387e2b98e700de87ef95c1faca3a963
Patrick Suter <patrickasuter@gmail.com>
2013-05-26 16:36:44 +0200

    perform deliveries in development env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,7 +31,7 @@ SGfM::Application.configure do
 
   # actionmailer redirect, check console for output
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.perform_deliveries = false
+  config.action_mailer.perform_deliveries = true
   #config.action_mailer.delivery_method = :sendmail #:smtp
   #config.action_mailer.default_url_options = { :host => 'localhost', :locale =>I18n.locale }
 
